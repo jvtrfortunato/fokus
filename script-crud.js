7e17e1c1b93467856ac57df9c18474b3fdd7bfaa
jvtrfortunato <joaovitor.fortunato@hotmail.com>
2026-03-16 14:41:31 -0300

    feat: adiciona e finaliza logica de selecao de tarefas

--- script-crud.js
+++ script-crud.js
@@ -3,16 +3,18 @@ const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textArea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
 const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+let tarefaSelecionada = null
 
 function atualizarTarefas() {
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
 }
 
 const limparFormulario = () => {
-    textArea.value = '';  // Limpe o conteúdo do textarea
-    formAdicionarTarefa.classList.add('hidden');  // Adicione a classe 'hidden' ao formulário para escondê-lo
+    textArea.value = '';
+    formAdicionarTarefa.classList.add('hidden');
 }
 
 function criarElementoTarefa(tarefa) {
@@ -36,9 +38,9 @@ function criarElementoTarefa(tarefa) {
     botao.classList.add('app_button-edit')
 
     botao.onclick = () => {
-        debugger
+        // debugger
         const novaDescricao = prompt("Qual é o novo nome da tarefa?")
-        console.log('Nova descrição da terefa:', novaDescricao)
+        // console.log('Nova descrição da terefa:', novaDescricao)
         if (novaDescricao) {
             paragrafo.textContent = novaDescricao
             tarefa.descricao = novaDescricao
@@ -53,6 +55,21 @@ function criarElementoTarefa(tarefa) {
     li.append(svg)
     li.append(paragrafo)
     li.append(botao)
+
+    li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active')
+            .forEach(elemento => {
+                elemento.classList.remove('app__section-task-list-item-active')
+            })
+        if (tarefaSelecionada == tarefa) {
+            paragrafoDescricaoTarefa.textContent = ''
+            tarefaSelecionada = null
+            return
+        }
+        tarefaSelecionada = tarefa
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao
+        li.classList.add('app__section-task-list-item-active')
+    }
 
     return li
 }
