da3c9ff53f72a0f4db1d177b41c3a4791d0f4848
jvtrfortunato <joaovitor.fortunato@hotmail.com>
2026-03-16 17:35:53 -0300

    feat: adiciona logica de remocao de todas as tarefas

--- script-crud.js
+++ script-crud.js
@@ -5,7 +5,8 @@ const ulTarefas = document.querySelector('.app__section-task-list')
 const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
 const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
 
-const btncRemoverConcluidas = document.querySelector('#btn-remover-concluidas')
+const btnRemoverConcluidas = document.querySelector('#btn-remover-concluidas')
+const btnRemoverTodas = document.querySelector('#btn-remover-todas')
 
 let tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 let tarefaSelecionada = null
@@ -119,11 +120,14 @@ document.addEventListener('FocoFinalizado', () => {
     }
 })
 
-btncRemoverConcluidas.onclick = () => {
-    const seletor = ".app__section-task-list-item-complete"
+const removerTarefas = (somenteCompletas) => {
+    const seletor = somenteCompletas ? ".app__section-task-list-item-complete" : ".app__section-task-list-item"
     document.querySelectorAll(seletor).forEach(elemento => {
         elemento.remove()
     })
-    tarefas = tarefas.filter(tarefa => !tarefa.completa)
+    tarefas = somenteCompletas ? tarefas.filter(tarefa => !tarefa.completa) : []
     atualizarTarefas()
 }
+
+btnRemoverConcluidas.onclick = () => removerTarefas(true)
+btnRemoverTodas.onclick = () => removerTarefas(false)
